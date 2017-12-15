b509dcb95b6c786ea11bd2c54e83219d9e5b1b60
github username <github email address>
2017-12-15 14:27:09 -0600

    created new comments class and table

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,7 @@ require 'pry'
 class Trip < ActiveRecord::Base
   belongs_to :user
   has_many :places
+  has_many :comments
 
   def places_attributes=(places_attributes)
     places_attributes.values.each do |places_attributes|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207224931) do
+ActiveRecord::Schema.define(version: 20171215201245) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "description"
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
