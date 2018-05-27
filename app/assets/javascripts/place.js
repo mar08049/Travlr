1cd1bc901ead281ef744af6e35263543073977a0
github username <github email address>
2018-05-27 07:27:56 -0400

    small changes to UI

--- app/assets/javascripts/place.js
+++ app/assets/javascripts/place.js
@@ -5,10 +5,10 @@ $(document).ready(function(){ // a page cannot be manipulated until document is 
 
 //Create a class object in ES6 syntax
 class Comment{
-  constructor(name, description, place_id) { //use constructor
+  constructor(name, description, placeID) { //use constructor
     this.name = name;    //use keys that match variables
     this.description = description;
-    this.place_id = place_id;
+    this.placeID = placeID;
   }
 }
 
@@ -16,9 +16,10 @@ class Comment{
 Comment.prototype.makeComment = function() { //prototype function on Comment class
   let html = ''    //set html = to empty string
   html += `<p class="comments">${this.description}</p>` //html can be empty or more
-  html += `<p class="comment_by">By: ${this.name}</p>`
+  html += `<p class="comment_by"> ${this.name}</p>`
   return html; //return the new html
 };
+
 
 //load comments index using json
  $(function(){
@@ -27,15 +28,18 @@ Comment.prototype.makeComment = function() { //prototype function on Comment cla
      $.getJSON(this.href).success(function(json){ //on success, append comment
        var $ul = $("div.comments ul"); //set ul var to the comments ul div
        $ul.html(""); //get the html contents of the variable
-       json.forEach(function(comment){ //for each json, perform function
+       json.forEach(comment => { //for each json, perform function
          $ul.append("<li>" + comment.description + "</li>"); // appends comment desc in li tags
-         $ul.append("<li>" + "-" + comment.name + "</li>" + "<br>" + "<br>" // appends comment name in li tags
-       );
+         $ul.append("<li>-" + comment.name + "</li><br><br>"); // appends comment name in li tags
        });
+
      });
      e.preventDefault();
    });
  });
+
+
+
 
 //create a new comment
 $(function(){ //$ is used for jquery functions
@@ -47,7 +51,7 @@ $(function(){ //$ is used for jquery functions
       data: $(e.target.form).serialize(),//creates text string of form
       dataType: "JSON"
     }).success(function(response){   //on success
-      let comment = new Comment(response.name, response.description, response.place_id);
+      let comment = new Comment(response.name, response.description, response.placeID);
       $("#comment_description").val("");// when successful post request, create new comment
       var $ul = $("div.comments ul"); //creates ul variable equal to the commment list div
       $ul.append(comment.makeComment()); //append new comment, call makeComment on comment
@@ -63,11 +67,9 @@ function nextTrip(){
       $(".tripInfo").text(data["name"]);//name text from tripInfo class
       var html = "";
       data["places"].forEach(function(place){
-
         html += "<p><a href='/trips/" + data.id + "/places/" + place.id + "'>" + place.name + "</a></p>"
-      })
+      });
       $(".placeInfo").html(html);
-
       $(".next_link").attr("data-attribute", data["id"]); // sets id for next link
       $(".previous_link").attr("data-attribute", data["id"] - 1); // sets id for previous link which is id -1
     })
@@ -85,7 +87,6 @@ function previousTrip(){
         html += "<p><a href='/trips/" + data.id + "/places/" + place.id + "'>" + place.name + "</a></p>"
       });
       $(".placeInfo").html(html);
-
       $(".previous_link").attr("data-attribute", data["id"]); // sets id for next link
       $(".next_link").attr("data-attribute", data["id"] + 1); // sets id for previous link which is id -1
     });
